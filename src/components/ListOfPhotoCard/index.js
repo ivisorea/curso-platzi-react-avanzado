955d5141ad86aaf0cdce8fc9415071edcaf50cc1
Ivis Orea <ivisorea@gmail.com>
2022-04-28 17:30:35 +0200

    ADD-Fetching photos

--- src/components/ListOfPhotoCard/index.js
+++ src/components/ListOfPhotoCard/index.js
@@ -1,17 +1,35 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
+import { Loader } from '../Loader'
 import { PhotoCard } from '../PhotoCard'
 
+const GET_PHOTOS = gql`
+  query getPhotos{
+    photos{
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+  `
+
 export const ListOfPhotoCard = () => {
+  const { loading, error, data } = useQuery(GET_PHOTOS)
+
+  if (loading) return <Loader />
+  if (error) return <p>Error :(</p>
+
   return (
     <>
       <ul>
-        {
-            [1, 2, 3, 4, 5].map(photo => (
-              <li key={photo}>
-                <PhotoCard />
-              </li>
-            ))
-        }
+        {data.photos.map(photo => (
+          <li key={photo.id}>
+            <PhotoCard {...photo} />
+          </li>
+        ))}
       </ul>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const client = new ApolloClient({
+  uri: 'https://curso-platzi-react-avanzado-nine-khaki.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 )
